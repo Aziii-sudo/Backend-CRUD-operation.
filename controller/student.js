84c3247f28ef0d77e98e5758f8490516170632b4
Azeemnizar <azeemnizar03@gmail.com>
2022-02-10 07:11:22 +1400

    on making delete request , delete from database

--- controller/student.js
+++ controller/student.js
@@ -2,6 +2,8 @@ const Std = require ('../models/student');
 const mongoose = require('mongoose');
 
 
+
+
 function create(req,res,next){
     let stdName    =req.body.stdName;
     let stdRollno  =req.body.stdRollno;
@@ -31,12 +33,21 @@ function view (req,res,next){
 function update(req,res,next){
     Std.findByIdAndUpdate(req.params.id,req.body, (err,std)=>{
         if(err) {
-            return res.status(500).send({error: "problem with Updating the students records"})
+            return res.status(500).send({error: "problem with Updating the student records"})
         };
-        res.send({sucess: "Updated your details"})
+        res.send({success: "Updated your details"})
+    })
+}
+function remove(req,res,next){
+    Std.findByIdAndDelete(req.params.id, (err,std)=>{
+        if(err){
+            return res.status(500).send({error: "problem with deleting the student records"})
+        };
+        res.send({success: "student profile deleted "})
     })
 }
 
 module.exports.create = create
 module.exports.view = view 
 module.exports.update = update
+module.exports.remove = remove
--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,7 @@ var router = express()
 const create = require('../controller/student')
 const view =require ('../controller/student')
 const update =require ('../controller/student')
+const remove = require ('../controller/student')
 const bodyparser = require('body-parser');
 
 
@@ -12,6 +13,7 @@ router.use(bodyparser.json())
 router.post('/create',create.create)
 router.get('/',view.view)
 router.patch('/:id',update.update)
+router.delete('/delete/:id',remove.remove)
 
 module.exports = router
 
